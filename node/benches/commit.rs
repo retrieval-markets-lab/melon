db92cb9d6999b6c73cda4a40dd24610d3c9fff67
dante <45801863+alexander-camuto@users.noreply.github.com>
2022-12-28 13:51:32 +0000

    feat: kzg commitments rust (#3)

---
+++ node/benches/commit.rs
@@ -0,0 +1,34 @@
+use blstrs::Scalar;
+use melon::kzg::polynomial::Polynomial;
+use melon::kzg::{setup, KZGParams, KZGProver};
+use pairing::group::ff::Field;
+use rand::{rngs::SmallRng, Rng, SeedableRng};
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+pub fn csprng_setup<const MAX_COEFFS: usize>() -> KZGParams {
+    let s: Scalar = rand::random::<u64>().into();
+    setup(s, MAX_COEFFS)
+}
+
+fn bench_commit<const NUM_COEFFS: usize>(c: &mut Criterion) {
+    let params = csprng_setup::<NUM_COEFFS>();
+    let mut rng = SmallRng::from_seed([42; 32]);
+    let mut coeffs = vec![Scalar::zero(); NUM_COEFFS];
+    for i in 0..NUM_COEFFS {
+        coeffs[i] = rng.gen::<u64>().into();
+    }
+    let polynomial = Polynomial::new(coeffs);
+    let prover = KZGProver::new(&params);
+
+    c.bench_function(format!("commit, degree {}", NUM_COEFFS - 1).as_str(), |b| {
+        b.iter(|| black_box(&prover).commit(black_box(&polynomial)))
+    });
+}
+
+criterion_group!(
+    name = commit;
+    config = Criterion::default();
+    targets = bench_commit<16>, bench_commit<64>, bench_commit<128>, bench_commit<256>, bench_commit<512>, bench_commit<1024>, bench_commit<2048>,  bench_commit<5096>
+);
+criterion_main!(commit);
---
+++ node/benches/poly_arithmetic.rs
@@ -0,0 +1,64 @@
+use blstrs::Scalar;
+use melon::kzg::polynomial::Polynomial;
+use pairing::group::ff::Field;
+use rand::{rngs::SmallRng, Rng, SeedableRng};
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn random_polynomial(rng: &mut SmallRng, n: usize) -> Polynomial {
+    let mut coeffs = vec![Scalar::zero(); n];
+    for coeff in coeffs.iter_mut().take(n) {
+        *coeff = rng.gen::<u64>().into();
+    }
+    Polynomial::new(coeffs)
+}
+
+fn poly_arithmetic<const NUM_COEFFS: usize>(c: &mut Criterion) {
+    let mut rng = SmallRng::from_seed([NUM_COEFFS as u8; 32]);
+    let f = random_polynomial(&mut rng, NUM_COEFFS);
+    let g = random_polynomial(&mut rng, NUM_COEFFS);
+
+    c.bench_function(format!("add, degree {}", NUM_COEFFS - 1).as_str(), |b| {
+        b.iter(|| black_box(f.clone()) + black_box(g.clone()));
+    });
+
+    c.bench_function(
+        format!("mul_naive, degree {}", NUM_COEFFS - 1).as_str(),
+        |b| {
+            b.iter(|| black_box(f.clone()) * black_box(g.clone()));
+        },
+    );
+
+    c.bench_function(
+        format!("mul_fft, degree {}", NUM_COEFFS - 1).as_str(),
+        |b| {
+            b.iter(|| black_box(f.clone()).fft_mul(black_box(&g)));
+        },
+    );
+
+    let g = random_polynomial(&mut rng, 2);
+
+    c.bench_function(
+        format!("long_division, degree {}", NUM_COEFFS - 1).as_str(),
+        |b| b.iter(|| black_box(f.clone()).long_division(&black_box(g.clone()))),
+    );
+
+    let mut xs = vec![Scalar::zero(); NUM_COEFFS - 1];
+    let mut ys = vec![Scalar::zero(); NUM_COEFFS - 1];
+    for i in 0..xs.len() {
+        xs[i] = Scalar::random(&mut rng);
+        ys[i] = Scalar::random(&mut rng);
+    }
+
+    c.bench_function(
+        format!("interpolation, degree {}", NUM_COEFFS - 1).as_str(),
+        |b| b.iter(|| Polynomial::lagrange_interpolation(xs.as_slice(), ys.as_slice())),
+    );
+}
+
+criterion_group!(
+    name = bench_poly_arithmetic;
+    config = Criterion::default();
+    targets = poly_arithmetic<16>, poly_arithmetic<64>, poly_arithmetic<128>, poly_arithmetic<256>, poly_arithmetic<512>, poly_arithmetic<1024>, poly_arithmetic<2048>, poly_arithmetic<5098>
+);
+criterion_main!(bench_poly_arithmetic);
